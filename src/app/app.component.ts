3e425d5d8b2363e14f9036e46f0f6fdb24cd1205
usualpolak <usualpolak@hotmail.com>
2025-02-25 13:00:31 +0100

    added header and navbar html/css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './header/header.component';
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [AboutComponent],
+  imports: [AboutComponent, HeaderComponent, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
