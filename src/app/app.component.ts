8cdc10ae0d3dc118ebaa78c3dd94744f2eff4e8f
usualpolak <usualpolak@hotmail.com>
2025-03-14 12:02:36 +0100

    Added contact form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,11 @@ import { SkillsListComponent } from "./skills/skills-list/skills-list.component"
 import { FormationsListComponent } from './formation/formations-list/formations-list.component';
 import { ExperiencesListComponent } from "./experience/experiences-list/experiences-list.component";
 import { PortfolioListComponent } from "./portfolio/portfolio-list/portfolio-list.component";
+import { ContactComponent } from "./contact/contact.component";
 
 @Component({
   selector: 'app-root',
-  imports: [AboutComponent, HeaderComponent, FooterComponent, NavBarComponent, SkillsListComponent, FormationsListComponent, ExperiencesListComponent, PortfolioListComponent],
+  imports: [AboutComponent, HeaderComponent, FooterComponent, NavBarComponent, SkillsListComponent, FormationsListComponent, ExperiencesListComponent, PortfolioListComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
+import { CommonModule, JsonPipe } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  fb = inject(FormBuilder)
 
+  form: FormGroup = this.fb.group({
+    form_subject: '',
+    form_name: '',
+    form_email: '',
+    form_message: ''
+  });
+
+  constructor() { }
+
+  async send() {
+    emailjs.init('vomkow1WMhU495RtB')
+    let response = await emailjs.send("service_18704ol", "template_5e61hs8", {
+      form_subject: this.form.value.form_name,
+      form_name: this.form.value.form_name,
+      form_email: this.form.value.form_email,
+      form_message: this.form.value.form_message,
+    });
+
+    alert('Message has been sent.');
+    this.form.reset();
+  }
 }
+
