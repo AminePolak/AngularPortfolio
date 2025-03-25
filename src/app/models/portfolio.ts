d5a228ed1e7c79689020424b07e59ae42b0ceddc
usualpolak <usualpolak@hotmail.com>
2025-03-25 07:19:36 +0100

    added title in portfolio li

--- src/app/models/portfolio.ts
+++ src/app/models/portfolio.ts
@@ -1,10 +1,12 @@
 export class Portfolio {
+    title!: string;
     image!: string;
     link!: string;
     githubLink?: string;
     description!: string;
 
-    constructor(image: string, link: string, description: string) {
+    constructor(title: string, image: string, link: string, description: string) {
+        this.title = title;
         this.image = image;
         this.link = link;
         this.description = description
